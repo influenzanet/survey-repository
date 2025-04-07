21ddab7e39986a86939bfe79a847c4e0ae92d28c
Clément Turbelin <clement.turbelin@upmc.fr>
2025-04-07 17:16:29 +0200

    change default ports in vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      "^/namespace":"http://127.0.0.1:8080",
-      "^/survey":"http://127.0.0.1:8080",
+      "^/namespace":"http://127.0.0.1:3100",
+      "^/survey":"http://127.0.0.1:3100",
     },
     headers: {
     },
